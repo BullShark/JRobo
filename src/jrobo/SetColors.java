dd75f7583ce9752d0dd64a187e09a13e3222f838
BullShark <goodbye300@aim.com>
2013-02-19 16:00:45 -0600

    Child class of Mirc, contains methods for special coloring, lines that contain more than one color code/attribute

---
+++ src/jrobo/SetColors.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jrobo;
+
+/**
+ *
+ * @author bullshark
+ */
+public class SetColors extends MircColors {
+  public String colorPirate(String msg) {
+    return msg;
+  }
+}
