8bcc5f4f7d1c8dc7b1157d068d612df05bbbfaa6
BullShark <goodbye300@aim.com>
2013-02-19 12:35:47 -0600

    Fixed imports

--- src/jrobo/Networking.java
+++ src/jrobo/Networking.java
@@ -19,18 +19,17 @@
 
 package jrobo;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.BufferedWriter;
-import java.io.BufferedReader;
-import java.io.IOException;
+import static java.lang.System.err;
+import static java.lang.System.out;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import static java.lang.System.out;
-import static java.lang.System.err;
 
 /**
  *
