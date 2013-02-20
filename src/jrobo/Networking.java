926d2ac0ab8c3230d2a94ab2fa5ad5f65134954f
TESLA <TESLA@TESLA-TOSH>
2013-02-20 23:44:38 +0500

    word wrap fix

--- src/jrobo/Networking.java
+++ src/jrobo/Networking.java
@@ -28,6 +28,8 @@ import static java.lang.System.err;
 import static java.lang.System.out;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
+import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -196,21 +198,70 @@ public class Networking {
    * This is used to prevent a message being truncated by IRC because
    * It exceeds MAXCHARS
    */
-  private String addNewLines(String command) {
-    int len = command.length();
-    String tmp = "";
-    if (len > MAXCHARS) {
-      // pos = position
-      for(int pos = 0; pos < len; pos+=400) {
-        if( (len - pos) <= 400) {
-          tmp += command.substring(pos);
-        } else {
-          tmp += command.substring(pos, pos+400) + '\n';
+private String addNewLines(String command) {
+        String[] lines = wrapText(command, 400);
+        String tmp = "";
+        for (int i = 0; i < lines.length; i++) {
+            tmp += lines[i] + "\n";
         }
-      }
-      return tmp;
-    } else {
-      return command;
+        return tmp;
     }
-  }
+
+    static String[] wrapText(String text, int len) {
+        // return empty array for null text
+        if (text == null) {
+            return new String[]{};
+        }
+
+        // return text if len is zero or less
+        if (len <= 0) {
+            return new String[]{text};
+        }
+
+        // return text if less than length
+        if (text.length() <= len) {
+            return new String[]{text};
+        }
+
+        char[] chars = text.toCharArray();
+        Vector lines = new Vector();
+        StringBuffer line = new StringBuffer();
+        StringBuffer word = new StringBuffer();
+
+        for (int i = 0; i < chars.length; i++) {
+            word.append(chars[i]);
+
+            if (chars[i] == ' ') {
+                if ((line.length() + word.length()) > len) {
+                    lines.add(line.toString());
+                    line.delete(0, line.length());
+                }
+
+                line.append(word);
+                word.delete(0, word.length());
+            }
+        }
+
+        // handle any extra chars in current word
+        if (word.length() > 0) {
+            if ((line.length() + word.length()) > len) {
+                lines.add(line.toString());
+                line.delete(0, line.length());
+            }
+            line.append(word);
+        }
+
+        // handle extra line
+        if (line.length() > 0) {
+            lines.add(line.toString());
+        }
+
+        String[] ret = new String[lines.size()];
+        int c = 0; // counter
+        for (Enumeration e = lines.elements(); e.hasMoreElements(); c++) {
+            ret[c] = (String) e.nextElement();
+        }
+
+        return ret;
+    }
 } //EOF class
