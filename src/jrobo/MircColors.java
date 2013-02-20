df5926093528416f94e6debd2b23ecabf2cc46b6
BullShark <goodbye300@aim.com>
2013-02-20 02:01:27 -0600

    Constant string variables with values for color/attribute codes

--- src/jrobo/MircColors.java
+++ src/jrobo/MircColors.java
@@ -17,17 +17,135 @@
  *
  */
 
+package jrobo;
+
 /*
  * This class will define constants and methods for sending colors to channels
  * That have /mode +c allowed in the channel
  *
  * This class could be defined simililar to
  * http://www.jibble.org/javadocs/pircbot/org/jibble/pircbot/Colors.html
+ * 
+ * Color codes
+ * http://www.weechat.org/files/doc/stable/weechat_user.en.html#command_line_colors
  */
+public class MircColors {
+   /**
+   * Removes all previously applied color and formatting attributes.
+   */
+  public static final String NORMAL = "\u000f";
 
-// See color chart at
-// http://www.weechat.org/files/doc/stable/weechat_user.en.html#command_line
-public enum MircColors {
-    
-  }
+
+  /**
+   * Bold text.
+   */
+  public static final String BOLD = "\u0002";
+  
+
+  /**
+   * Underlined text.
+   */
+  public static final String UNDERLINE = "\u001f";
+
+
+  /**
+   * Reversed text (may be rendered as italic text in some clients).
+   */
+  public static final String REVERSE = "\u0016";
+  
+
+  /**
+   * White colored text.
+   */
+  public static final String WHITE = "\u000300";
+  
+
+  /**
+   * Black colored text.
+   */
+  public static final String BLACK = "\u000301";
+  
+
+  /**
+   * Dark blue colored text.
+   */
+  public static final String DARK_BLUE = "\u000302";
+  
+
+  /**
+   * Dark green colored text.
+   */
+  public static final String DARK_GREEN = "\u000303";
+  
+
+  /**
+   * Red colored text.
+   */
+  public static final String RED = "\u000304";
+  
+
+  /**
+   * Brown colored text.
+   */
+  public static final String BROWN = "\u000305";
+  
+
+  /**
+   * Purple colored text.
+   */
+  public static final String PURPLE = "\u000306";
+  
+
+  /**
+   * Olive colored text.
+   */
+  public static final String OLIVE = "\u000307";
+  
+
+  /**
+   * Yellow colored text.
+   */
+  public static final String YELLOW = "\u000308";
+  
+
+  /**
+   * Green colored text.
+   */
+  public static final String GREEN = "\u000309";
+  
+
+  /**
+   * Teal colored text.
+   */
+  public static final String TEAL = "\u000310";
+  
+
+  /**
+   * Cyan colored text.
+   */
+  public static final String CYAN = "\u000311";
+  
+
+  /**
+   * Blue colored text.
+   */
+  public static final String BLUE = "\u000312";
+  
+
+  /**
+   * Magenta colored text.
+   */
+  public static final String MAGENTA = "\u000313";
+  
+
+  /**
+   * Dark gray colored text.
+   */
+  public static final String DARK_GRAY = "\u000314";
+  
+
+  /**
+   * Light gray colored text.
+   */
+  public static final String LIGHT_GRAY = "\u000315";
 }
