f761dea071dd4b1e8db250807cfc725f12a777f2
BinaryStroke <BinaryStroke@null.net>
2013-02-23 20:18:47 +0300

    reverted a change was done by auto merge

--- src/jrobo/BotCommand.java
+++ src/jrobo/BotCommand.java
@@ -266,7 +266,6 @@ public class BotCommand {
 //TODO Re-implement all the use wunderground.net
 }
 
-/* Sends a your momma joke to a random user */
   private void mumHelper() {
       
       Jokes joke =new Jokes(this.connection);
