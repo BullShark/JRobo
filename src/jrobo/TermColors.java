7bbc586ab2f31f700bcd7218093dd6718dec91f8
BullShark <goodbye300@aim.com>
2013-03-22 09:10:01 -0500

    Updated Copylefts

--- src/jrobo/TermColors.java
+++ src/jrobo/TermColors.java
@@ -3,6 +3,8 @@
  *
  * Copyright (C) <2013> <Christopher Lemire>
  * Copyright (C) <2013> <Travis Mosley>
+ * Copyright (C) <2013> <BinaryStroke>
+ * Copyright (C) <2013> <Muhammad Sajid>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
