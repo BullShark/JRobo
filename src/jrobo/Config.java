22b769ad401bcb8129dd86a1ff1e874bf27b5359
BullShark <goodbye300@aim.com>
2013-08-13 00:09:05 -0500

    Removed unused variable and NotConfig

--- src/jrobo/Config.java
+++ src/jrobo/Config.java
@@ -26,104 +26,123 @@ package jrobo;
  * @since 2013-02-18
  */
 public class Config {
-    private String name;
-    private String password;
-    private String[] masters;
-    private String[] hostmasks; //TODO Remove when masters is working
-    private char command_symbol;
-    private String network;
-    private String channel;
-    private String wunderground_key;
-    
-    /**
-     * 
-     * @return The bot's name
-     * @since 2013-02-18
-     */
-    public String getName () {
-        return name;
-    }
-    
-    /**
-     * 
-     * @return The bot's password
-     * @since 2013-02-18
-     */
-    public String getPass () {
-        return password;
-    }
-    
-    /**
-     * 
-     * @return The bot's masters nicknames in an array
-     * @since 2013-02-18
-     */
-    public String[] getMasters () {
-        return masters;
-    }
-    
-    /**
-     * 
-     * @return The hostmasks in an array
-     */
-    public String[] getHostmasks() {
-        return hostmasks;
-    }
-    
-    /**
-     * 
-     * @return The bot's command symbol
-     * @since 2013-02-18
-     */
-    public char getCmdSymb () {
-        return command_symbol;
-    }
-    
-    /**
-     * 
-     * @return The network to be connected
-     * @since 2013-02-18
-     */
-    public String getNetwork () {
-        return network;
-    }
-    
-    /**
-     * Represents the current channel
-     * 
-     * @return The channel to be joined
-     * @since 2013-02-18
-     */
-    public String getChannel () {
-        return channel;
-    }
 
-    /**
-     * Represents the current channel
-     * 
-     * @param chan 
-     * @since 2013-08-12
-     */
-    public void setChannel (String chan) {
-        channel = chan;
-    }
-    
-    /**
-     * 
-     * @return The weatherUndergound API key
-     * @since 2013-02-18
-     */
-    public String getWundergroundKey (){
-        return wunderground_key;
-    }
-    
-    /**
-     * 
-     * @return The configured name
-     * @since 2013-02-18
-     */
-    @Override
-    public String toString() {
-        return "My name is " + name;
-    }
+  private String name;
+  private String password;
+  private String[] masters;
+  private char command_symbol;
+  private String network;
+  private String channel;
+  private String wunderground_key;
+
+  /**
+   *
+   * @return The bot's name
+   * @since 2013-02-18
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   *
+   * @return The bot's password
+   * @since 2013-02-18
+   */
+  public String getPass() {
+    return password;
+  }
+
+  /**
+   *
+   * @return The bot's masters nicknames in an array
+   * @since 2013-02-18
+   */
+  public String[] getMasters() {
+    return masters;
+  }
+
+  /**
+   *
+   * @return The hostmasks in an array
+   */
+  public String[] getHostmasks() {
+    return hostmasks;
+  }
+
+  /**
+   *
+   * @return The bot's command symbol
+   * @since 2013-02-18
+   */
+  public char getCmdSymb() {
+    return command_symbol;
+  }
+
+  /**
+   *
+   * @return The network to be connected
+   * @since 2013-02-18
+   */
+  public String getNetwork() {
+    return network;
+  }
+
+  /**
+   * Represents the current channel
+   *
+   * @return The channel to be joined
+   * @since 2013-02-18
+   */
+  public String getChannel() {
+    return channel;
+  }
+
+  /**
+   * Represents the current channel
+   *
+   * @param chan
+   * @since 2013-08-12
+   */
+  public void setChannel(String chan) {
+    channel = chan;
+  }
+
+  /**
+   *
+   * @return The weatherUndergound API key
+   * @since 2013-02-18
+   */
+  public String getWundergroundKey() {
+    return wunderground_key;
+  }
+
+  /**
+   *
+   * @return The configured name
+   * @since 2013-02-18
+   */
+  @Override
+  public String toString() {
+    return "My name is " + name;
+  }
+  private String baseChan;
+
+  /**
+   * Channel from where the bot began moving
+   *
+   * @return
+   */
+  public String getBaseChan() {
+    return baseChan;
+  }
+
+  /**
+   * Channel from where the bot began moving
+   *
+   * @param baseChan
+   */
+  public void setBaseChan(String baseChan) {
+    this.baseChan = baseChan;
+  }
 }
