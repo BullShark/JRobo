e1b96a2760ab882e24f68d9f23f3b136fb2669a1
Joseph Kinsella <jkinsella@spiryx.net>
2013-09-27 20:04:57 -0400

    OpCommands object. (So far only part/goto, but should ultimately have other op-like commands in it.)

---
+++ src/jrobo/command/OpCommands.java
@@ -0,0 +1,25 @@
+package jrobo.command;
+import jrobo.JRobo;
+
+@JRoboCommand(author="jotaki",date="2013/09/27",command="join,goto")
+public class OpCommands extends BotCommand {
+  public OpCommands() {
+    super();
+  }
+
+  public void execute(String target, String[] args) {
+    if(args.length == 0)
+      help(target);
+
+    switch(inputCommand) {
+      case "goto":
+        part(target);
+      case "join":
+        join(args[0]);
+    }
+  }
+
+  public void help(String target) {
+    privmsg(target, "Usage: " + inputCommand + " <channel>");
+  }
+}
