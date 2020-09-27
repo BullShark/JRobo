4071bfef3470187b5db8825cbd26048d62a76571
Chris Lemire <goodbye300@aim.com>
2020-09-27 02:21:22 -0500

    Add newlines in toString()

--- src/jrobo/Config.java
+++ src/jrobo/Config.java
@@ -145,13 +145,13 @@ public class Config {
    */
   @Override
   public String toString() {
-    return "My name is " + name
-	    + ", password is " + password.replaceAll(".", "*")
-	    + ", masters are " + Arrays.toString(masters)
-	    + ", command_symbol is " + command_symbol
-	    + ", network is " + network
-	    + ", channel is " + channel
-	    + ", openweathermap_apikey is " + openweathermap_apikey.replaceAll(".", "*")
-	    + ", baseChan is " + baseChan;
+    return "My name is " + name + "\n"
+	    + ", password is " + password.replaceAll(".", "*") + "\n"
+	    + ", masters are " + Arrays.toString(masters) + "\n"
+	    + ", command_symbol is " + command_symbol + "\n"
+	    + ", network is " + network + "\n"
+	    + ", channel is " + channel + "\n"
+	    + ", openweathermap_apikey is " + openweathermap_apikey.replaceAll(".", "*") + "\n"
+	    + ", baseChan is " + baseChan + "\n";
   }
 }
