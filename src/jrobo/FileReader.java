e21714ff6f87f9a21192717da32f594ea5e3e455
Chris Lemire <goodbye300@aim.com>
2020-10-03 02:35:44 -0500

    Exception handling for json syntax errors Replace everything that can be replaced using try-with-resources Created static boolean ranOnce that initializes as false, changes to true when the Config.json is read once, and prevents it from being read again

--- src/jrobo/FileReader.java
+++ src/jrobo/FileReader.java
@@ -19,6 +19,7 @@
 package jrobo;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -40,25 +41,28 @@ public class FileReader {
 	/* User-defined Classes */
 	private static Config config;
 
+	/* Miscellaneous */
+	private static boolean ranOnce = false;
+
 	public FileReader() {
 		configFile = "Config.json";
 		config = null;
 	}
 
 	/**
-	 * Opens a resource file inside the package and fills the passed ArrayList
-	 * 
+	 * Opens a resource file inside the package and fills the passed
+	 * ArrayList
+	 *
 	 * @param fileName The file name inside the package to be opened
 	 * @param listArr The array list to store the file lines to
 	 * @return A true on success and false on failure
 	 */
 	public boolean fileToArrayList(final String fileName, ArrayList<String> listArr) {
-		BufferedReader br;
-		try {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(FileReader.class.getResourceAsStream(fileName)));) {
 			Thread.dumpStack();
-			out.println("[+++]\tReading File (" + fileName + ")");
 
-			br = new BufferedReader(new InputStreamReader(FileReader.class.getResourceAsStream(fileName)));
+			if(fileName.equals(configFile)) { ranOnce = true; }
+			out.println("[+++]\tReading File (" + fileName + ")");
 
 			String line;
 
@@ -67,8 +71,6 @@ public class FileReader {
 			}
 		} catch (IOException ex) {
 			ex.printStackTrace();
-			br = null;
-			return false;
 		}
 
 		Collections.shuffle(listArr);
@@ -83,81 +85,92 @@ public class FileReader {
 	 * @since 2013-02-18
 	 */
 	public static Config getConfig() {
-		
-		/* Should only be null if this method has not already been ran once or the constructor has never been called */
-		if(config != null) {
+
+		/* 
+		 * Should only be null if this method has not already been ran once 
+		 * Or the constructor has never been called
+		 */
+		if (config != null || ranOnce) {
 			//Thread.dumpStack();
-			out.println("[+++]\tReusing Config because it's != null");
+			out.println("[+++]\tReusing Config because it's != null or " + configFile + " has already been read once");
 			return config;
 		}
 
-		Thread.dumpStack();
-		out.println("[+++]\tReading Configuration File (Config.json)");
+		out.println("[+++]\tReading Configuration File (" + configFile + ")");
+		try (
+			InputStream fileStream = FileReader.class.getResourceAsStream(configFile);
+			InputStreamReader fileStreamReader = new InputStreamReader(fileStream);
+			BufferedReader br = new BufferedReader(fileStreamReader);
+		) {
+			Thread.dumpStack();
 
-	        InputStream fileStream = FileReader.class.getResourceAsStream(configFile);
+			if (fileStream == null) {
+				err.println("[+++]\tError: " + configFile + " was not found");
+				System.exit(1);
+			}
 
-		if(fileStream == null) {
-			err.println("[+++]\tError: " + configFile + " was not found");
-			System.exit(1);
-		}
+			String line, json;
 
-		InputStreamReader fileStreamReader = new InputStreamReader(fileStream);
-		BufferedReader br = new BufferedReader(fileStreamReader);
-		String line, json;
+			json = "";
 
-		json = "";
-		try {
-			while((line = br.readLine()) != null) {
+			while ((line = br.readLine()) != null) {
 				json += line;
 			}
 
-			br.close();
+			Gson gson = new Gson();
+			config = gson.fromJson(json, Config.class);
 
-		} catch (IOException ex) {
-			err.println("[+++]\tError:" + ex.getMessage());
+			ranOnce = true;
+
+		} catch (IOException | JsonSyntaxException ex) {
 			ex.printStackTrace();
 			System.exit(1);
 		}
 
-		Gson gson = new Gson();
-		config = gson.fromJson(json, Config.class);
 
 		// Verifiying important settings for connection
-		if (config.getName() == null) {
+		if (config.getName()
+			== null) {
 			err.println("[+++]\tError: Unable to find bot's Nickname");
 			System.exit(1);
 		}
 
-		if (config.getPass() == null) {
+		if (config.getPass()
+			== null) {
 			err.println("[+++]\tError: Unable to find bot's Password");
 			System.exit(1);
 		}
 
-		if (config.getMasters() == null) {
+		if (config.getMasters()
+			== null) {
 			err.println("[+++]\tError: Unable to find bot's Masters");
 			System.exit(1);
 		}
 
-		if (config.getCmdSymb() == '\u0000') {
+		if (config.getCmdSymb()
+			== '\u0000') {
 			err.println("[+++]\tError: Unable to find bot's Command Symbol");
 			System.exit(1);
 		}
 
-		if (config.getNetwork() == null) {
+		if (config.getNetwork()
+			== null) {
 			err.println("[+++]\tError: Unable to find bot's Network");
 			System.exit(1);
 		}
 
-		if (config.getChannel() == null) {
+		if (config.getChannel()
+			== null) {
 			err.println("[+++]\tError: Unable to find bot's Channel");
 			System.exit(1);
 		}
 
-		if (config.getOpenWeatherMapKey() == null) {
+		if (config.getOpenWeatherMapKey()
+			== null) {
 			err.println("[+++]\tError: Unable to find bot's OpenWeatherMap API Key");
 			//System.exit(1);
 		}
 
 		return config;
-	}
+	} // EOF getConfig()
 } // EOF class
