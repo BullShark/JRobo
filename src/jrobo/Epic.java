e35e9f45f63fbd2f3303cb39825582c20de02556
Chris Lemire <goodbye300@aim.com>
2020-10-05 05:43:10 -0500

    Remove unused imports

--- src/jrobo/Epic.java
+++ src/jrobo/Epic.java
@@ -25,10 +25,8 @@ import com.google.gson.reflect.TypeToken;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.lang.reflect.Type;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 
 /**
