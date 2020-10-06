34d450d20f087ba2a784f8a7dd37ddd6d4435828
Chris Lemire <goodbye300@aim.com>
2020-10-06 08:24:33 -0500

    Removed unused imports

--- src/main/java/jrobo/Weather.java
+++ src/main/java/jrobo/Weather.java
@@ -28,9 +28,9 @@ import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
