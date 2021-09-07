a10de70a5e62622e3be739d3ad44c7ffa8a60d22
Christopher Lemire <goodbye300@aim.com>
2021-09-07 14:56:26 -0500

    Merged

--- src/main/java/jrobo/FileReader.java
+++ src/main/java/jrobo/FileReader.java
@@ -21,11 +21,17 @@ package jrobo;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import static java.lang.System.err;
 import static java.lang.System.out;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.logging.Level;
@@ -37,144 +43,146 @@ import java.util.logging.Logger;
  */
 public class FileReader {
 
-	/* Standard Java API Classes */
-	private static String CONFIGFILE;
+    /* Standard Java API Classes */
+    private static String CONFIGFILE;
 
-	/* User-defined Classes */
-	private static Config config;
+    /* User-defined Classes */
+    private static Config config;
 
-	/* Miscellaneous */
-	private static boolean ranOnce = false;
+    /* Miscellaneous */
+    private static boolean ranOnce = false;
 
-	public FileReader() {
-		CONFIGFILE = "Config.json";
-		config = null;
-	}
+    public FileReader() {
+        CONFIGFILE = "Config.json";
+        config = null;
+    }
 
-	/**
-	 * Opens a resource file inside the package and fills the passed
-	 * ArrayList
-	 *
-	 * @param FILENAME The file name inside the package to be opened
-	 * @param LISTARR The array list to store the file lines to
-	 * @return A true on success and false on failure
-	 */
-	protected boolean fileToArrayList(final String FILENAME, final ArrayList<String> LISTARR) {
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(FileReader.class.getResourceAsStream(FILENAME)));) {
-			Thread.dumpStack();
+    /**
+     * Opens a resource file inside the package and fills the passed ArrayList
+     *
+     * @param FILENAME The file name inside the package to be opened
+     * @param LISTARR The array list to store the file lines to
+     * @return A true on success and false on failure
+     */
+    protected boolean fileToArrayList(final String FILENAME, final ArrayList<String> LISTARR) {
+        
+        try ( BufferedReader br = new BufferedReader(new InputStreamReader(FileReader.class.getResourceAsStream(FILENAME)));) {
+            Thread.dumpStack();
 
-			if(FILENAME.equals(CONFIGFILE)) { ranOnce = true; }
-			out.println("[+++]\tReading File (" + FILENAME + ")");
+            if (FILENAME.equals(CONFIGFILE)) {
+                ranOnce = true;
+            }
 
-			String line;
+            out.println("[+++]\tReading File (" + FILENAME + ")");
 
-			while ((line = br.readLine()) != null) {
-				LISTARR.add(line);
-			}
+            String line;
 
-		} catch (IOException ex) {
-			Logger.getLogger(FileReader.class.getName()).log(Level.SEVERE, null, ex);
+            while ((line = br.readLine()) != null) {
+                LISTARR.add(line);
+            }
 
-		}
+        } catch (IOException ex) {
+            Logger.getLogger(FileReader.class.getName()).log(Level.SEVERE, null, ex);
 
-		Collections.shuffle(LISTARR);
+        }
 
-		return true;
-	}
+        Collections.shuffle(LISTARR);
 
-	/**
-	 * Gets the data from the configuration file
-	 *
-	 * @return Returns a Config object, with the settings from Config.json
-	 * @since 2013-02-18
-	 */
-	protected static Config getConfig() {
+        return true;
+    }
 
-		/* 
+    /**
+     * Gets the data from the configuration file
+     *
+     * @return Returns a Config object, with the settings from Config.json
+     * @since 2013-02-18
+     */
+    protected static Config getConfig() {
+
+        /* 
 		 * Should only be null if this method has not already been ran once 
 		 * Or the constructor has never been called
-		 */
-		if (config != null || ranOnce) {
-			//Thread.dumpStack();
-			out.println("[+++]\tReusing Config because it's != null or " + CONFIGFILE + " has already been read once");
-			return config;
-		}
+         */
+        if (config != null || ranOnce) {
+            //Thread.dumpStack();
+            out.println("[+++]\tReusing Config because it's != null or " + CONFIGFILE + " has already been read once");
+            return config;
+        }
 
-		out.println("[+++]\tReading Configuration File (" + CONFIGFILE + ")");
-		try (
-			InputStream fileStream = FileReader.class.getResourceAsStream(CONFIGFILE);
-			InputStreamReader fileStreamReader = new InputStreamReader(fileStream);
-			BufferedReader br = new BufferedReader(fileStreamReader);
-		) {
-			Thread.dumpStack();
+        out.println("[+++]\tReading Configuration File (" + CONFIGFILE + ")");
+        
+        System.out.println("[+++]\t" + new File(CONFIGFILE).getAbsolutePath());
+        
+        try (
+                 InputStream fileStream = FileReader.class.getResourceAsStream(CONFIGFILE);  InputStreamReader fileStreamReader = new InputStreamReader(fileStream);  BufferedReader br = new BufferedReader(fileStreamReader);) {
+            Thread.dumpStack();
 
-			if (fileStream == null) {
-				err.println("[+++]\tError: " + CONFIGFILE + " was not found");
-				System.exit(1);
-			}
+            if (fileStream == null) {
+                err.println("[+++]\tError: " + CONFIGFILE + " was not found");
+                System.exit(1);
+            }
 
-			String line, json;
+            String line, json;
 
-			json = "";
+            json = "";
 
-			while ((line = br.readLine()) != null) {
-				json += line;
-			}
+            while ((line = br.readLine()) != null) {
+                json += line;
+            }
 
-			Gson gson = new Gson();
-			config = gson.fromJson(json, Config.class);
+            Gson gson = new Gson();
+            config = gson.fromJson(json, Config.class);
 
-			ranOnce = true;
+            ranOnce = true;
 
-		} catch (IOException | JsonSyntaxException ex) {
-			Logger.getLogger(FileReader.class.getName()).log(Level.SEVERE, null, ex);
-			System.exit(1);
-		}
+        } catch (IOException | JsonSyntaxException ex) {
+            Logger.getLogger(FileReader.class.getName()).log(Level.SEVERE, null, ex);
+            System.exit(1);
+        }
 
+        // Verifiying important settings for connection
+        if (config.getName()
+                == null) {
+            err.println("[+++]\tError: Unable to find bot's Nickname");
+            System.exit(1);
+        }
 
-		// Verifiying important settings for connection
-		if (config.getName()
-			== null) {
-			err.println("[+++]\tError: Unable to find bot's Nickname");
-			System.exit(1);
-		}
+        if (config.getPass()
+                == null) {
+            err.println("[+++]\tError: Unable to find bot's Password");
+            System.exit(1);
+        }
 
-		if (config.getPass()
-			== null) {
-			err.println("[+++]\tError: Unable to find bot's Password");
-			System.exit(1);
-		}
+        if (config.getMasters()
+                == null) {
+            err.println("[+++]\tError: Unable to find bot's Masters");
+            System.exit(1);
+        }
 
-		if (config.getMasters()
-			== null) {
-			err.println("[+++]\tError: Unable to find bot's Masters");
-			System.exit(1);
-		}
+        if (config.getCmdSymb()
+                == '\u0000') {
+            err.println("[+++]\tError: Unable to find bot's Command Symbol");
+            System.exit(1);
+        }
 
-		if (config.getCmdSymb()
-			== '\u0000') {
-			err.println("[+++]\tError: Unable to find bot's Command Symbol");
-			System.exit(1);
-		}
+        if (config.getNetwork()
+                == null) {
+            err.println("[+++]\tError: Unable to find bot's Network");
+            System.exit(1);
+        }
 
-		if (config.getNetwork()
-			== null) {
-			err.println("[+++]\tError: Unable to find bot's Network");
-			System.exit(1);
-		}
+        if (config.getChannel()
+                == null) {
+            err.println("[+++]\tError: Unable to find bot's Channel");
+            System.exit(1);
+        }
 
-		if (config.getChannel()
-			== null) {
-			err.println("[+++]\tError: Unable to find bot's Channel");
-			System.exit(1);
-		}
+        if (config.getOpenWeatherMapKey()
+                == null) {
+            err.println("[+++]\tError: Unable to find bot's OpenWeatherMap API Key");
+            //System.exit(1);
+        }
 
-		if (config.getOpenWeatherMapKey()
-			== null) {
-			err.println("[+++]\tError: Unable to find bot's OpenWeatherMap API Key");
-			//System.exit(1);
-		}
-
-		return config;
-	} // EOF getConfig()
+        return config;
+    } // EOF getConfig()
 } // EOF class
