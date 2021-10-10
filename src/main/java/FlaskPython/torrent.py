9bdff7c1b071ecc950f97c6f4f9006425462f33f
Christopher Lemire <goodbye300@aim.com>
2021-10-10 16:27:56 -0500

    Lots of imports

--- src/main/java/FlaskPython/torrent.py
+++ src/main/java/FlaskPython/torrent.py
@@ -4,11 +4,24 @@ import random
 import string
 from flask import request, jsonify
 from bs4 import BeautifulSoup
+
+from __future__ import with_statement                                                        
+  
+import contextlib
+  
 try:
     from urllib.parse import urlencode          
   
 except ImportError:
     from urllib import urlencode
+  
+try:
+    from urllib.request import urlopen
+  
+except ImportError:
+    from urllib2 import urlopen
+  
+import sys
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
