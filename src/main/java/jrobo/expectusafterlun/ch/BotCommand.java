d4bd8b6373083a2436d5a2e1288c57abaf35a5a5
Christopher Lemire <goodbye300@aim.com>
2021-10-29 01:14:58 -0500

    That's one big mama jama

--- src/main/java/jrobo/expectusafterlun/ch/BotCommand.java
+++ src/main/java/jrobo/expectusafterlun/ch/BotCommand.java
@@ -372,7 +372,8 @@ public class BotCommand {
 	        		helpWrapper(cmd);
         		} else {
 
-			CONNECTION.msgChannel(CONFIG.getChannel(), new Epic().getFormattedEpicSummary(true, -1), 2000);
+			CONNECTION.msgChannel(CONFIG.getChannel(), new Epic().getFormattedEpicSummary(true, -1), true, MircColors.BOLD +MircColors.CYAN, 2000);
+//			CONNECTION.msgChannel(CONFIG.getChannel(), new Epic().getFormattedEpicSummary(true, -1), 2000);
 		}
 	}
 
