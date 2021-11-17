427c969bc9d92fbdd204072dc5a721444dc263b0
Christopher Lemire <goodbye300@aim.com>
2021-11-16 22:48:24 -0600

    Added many more color and attribute codes

--- src/main/java/jrobo/expectusafterlun/ch/TermColors.java
+++ src/main/java/jrobo/expectusafterlun/ch/TermColors.java
@@ -27,10 +27,18 @@ package jrobo.expectusafterlun.ch;
  */
 public class TermColors {
 
+	/*******************************************************************
+	 * Reset
+	 *******************************************************************/
+
 	 /**
 	  * Removes all previously applied color and formatting attributes.
 	  */
 	public static final String ANSI_RESET = "\u001B[0m";
+
+	/*******************************************************************
+	 * Regular Colors
+	 *******************************************************************/
 
 	/**
 	 * Black colored text.
@@ -72,6 +80,36 @@ public class TermColors {
 	 */
 	public static final String ANSI_WHITE = "\u001B[37m";
 
+	/*******************************************************************
+	 * Bold
+	 *******************************************************************/
+
+	public static final String BLACK_BOLD = "\033[1;30m";  // BLACK
+	public static final String RED_BOLD = "\033[1;31m";    // RED
+	public static final String GREEN_BOLD = "\033[1;32m";  // GREEN
+	public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW
+	public static final String BLUE_BOLD = "\033[1;34m";   // BLUE
+	public static final String PURPLE_BOLD = "\033[1;35m"; // PURPLE
+	public static final String CYAN_BOLD = "\033[1;36m";   // CYAN
+	public static final String WHITE_BOLD = "\033[1;37m";  // WHITE
+
+	/*******************************************************************
+	 * Underline
+	 *******************************************************************/
+
+	public static final String BLACK_UNDERLINED = "\033[4;30m";  // BLACK
+	public static final String RED_UNDERLINED = "\033[4;31m";    // RED
+	public static final String GREEN_UNDERLINED = "\033[4;32m";  // GREEN
+	public static final String YELLOW_UNDERLINED = "\033[4;33m"; // YELLOW
+	public static final String BLUE_UNDERLINED = "\033[4;34m";   // BLUE
+	public static final String PURPLE_UNDERLINED = "\033[4;35m"; // PURPLE
+	public static final String CYAN_UNDERLINED = "\033[4;36m";   // CYAN
+	public static final String WHITE_UNDERLINED = "\033[4;37m";  // WHITE
+
+	/*******************************************************************
+	 * Background
+	 *******************************************************************/
+
 	/**
 	 * Black colored background.
 	 */
@@ -112,6 +150,50 @@ public class TermColors {
 	 */
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	/*******************************************************************
+	 * High Intensity
+	 *******************************************************************/
+
+	public static final String BLACK_BRIGHT = "\033[0;90m";  // BLACK
+	public static final String RED_BRIGHT = "\033[0;91m";    // RED
+	public static final String GREEN_BRIGHT = "\033[0;92m";  // GREEN
+	public static final String YELLOW_BRIGHT = "\033[0;93m"; // YELLOW
+	public static final String BLUE_BRIGHT = "\033[0;94m";   // BLUE
+	public static final String PURPLE_BRIGHT = "\033[0;95m"; // PURPLE
+	public static final String CYAN_BRIGHT = "\033[0;96m";   // CYAN
+	public static final String WHITE_BRIGHT = "\033[0;97m";  // WHITE
+
+	/*******************************************************************
+	 * Bold High Intensity
+	 *******************************************************************/
+
+	public static final String BLACK_BOLD_BRIGHT = "\033[1;90m"; // BLACK
+	public static final String RED_BOLD_BRIGHT = "\033[1;91m";   // RED
+	public static final String GREEN_BOLD_BRIGHT = "\033[1;92m"; // GREEN
+	public static final String YELLOW_BOLD_BRIGHT = "\033[1;93m";// YELLOW
+	public static final String BLUE_BOLD_BRIGHT = "\033[1;94m";  // BLUE
+	public static final String PURPLE_BOLD_BRIGHT = "\033[1;95m";// PURPLE
+	public static final String CYAN_BOLD_BRIGHT = "\033[1;96m";  // CYAN
+	public static final String WHITE_BOLD_BRIGHT = "\033[1;97m"; // WHITE
+
+	/*******************************************************************
+	 * High Intensity backgrounds
+	 *******************************************************************/
+
+	public static final String BLACK_BACKGROUND_BRIGHT = "\033[0;100m";// BLACK
+	public static final String RED_BACKGROUND_BRIGHT = "\033[0;101m";// RED
+	public static final String GREEN_BACKGROUND_BRIGHT = "\033[0;102m";// GREEN
+	public static final String YELLOW_BACKGROUND_BRIGHT = "\033[0;103m";// YELLOW
+	public static final String BLUE_BACKGROUND_BRIGHT = "\033[0;104m";// BLUE
+	public static final String PURPLE_BACKGROUND_BRIGHT = "\033[0;105m"; // PURPLE
+	public static final String CYAN_BACKGROUND_BRIGHT = "\033[0;106m";  // CYAN
+	public static final String WHITE_BACKGROUND_BRIGHT = "\033[0;107m";   // WHITE
+
+
+
+	
+
+
 	/* TODO Use me, 3 methods below */
 
 	/**
@@ -143,7 +225,8 @@ public class TermColors {
 	}
 
 	/**
-	 * This method means to change the color, it only needs to be done here, not in 150 different places, change only once
+	 * This method means to change the color, it only needs to be done here,
+	 * not in 150 different places, change only once
 	 *
 	 * @param MSG The console server input message to color
 	 * @return The colored server input line
@@ -151,7 +234,7 @@ public class TermColors {
 	 */
 	public static String colorIn(final String MSG) {
 
-			return String.format(TermColors.ANSI_BLUE + "[---]" + TermColors.ANSI_RESET + "\t%s", MSG );
+		return String.format(TermColors.ANSI_BLUE + "[---]" + TermColors.ANSI_RESET + "\t%s", MSG);
 	}
 
 	/**
