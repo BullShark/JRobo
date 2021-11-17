b54a23072d9765c096ce1164e891dbc1f4ecf1d7
Christopher Lemire <goodbye300@aim.com>
2021-11-16 23:01:10 -0600

    Finished / completed

--- src/main/java/jrobo/expectusafterlun/ch/TermColors.java
+++ src/main/java/jrobo/expectusafterlun/ch/TermColors.java
@@ -87,42 +87,42 @@ public class TermColors {
 	/**
 	 * Black bold colored text.
 	 */
-	public static final String BLACK_BOLD = "\033[1;30m";  // BLACK
+	public static final String BLACK_BOLD = "\033[1;30m";
 
 	/**
 	 * Red bold colored text.
 	 */
-	public static final String RED_BOLD = "\033[1;31m";    // RED
+	public static final String RED_BOLD = "\033[1;31m";
 
 	/**
 	 * Green bold colored text.
 	 */
-	public static final String GREEN_BOLD = "\033[1;32m";  // GREEN
+	public static final String GREEN_BOLD = "\033[1;32m";
 
 	/**
 	 * Yellow bold colored text.
 	 */
-	public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW
+	public static final String YELLOW_BOLD = "\033[1;33m";
 
 	/**
 	 * Blue bold colored text.
 	 */
-	public static final String BLUE_BOLD = "\033[1;34m";   // BLUE
+	public static final String BLUE_BOLD = "\033[1;34m";
 
 	/**
 	 * Purple bold colored text.
 	 */
-	public static final String PURPLE_BOLD = "\033[1;35m"; // PURPLE
+	public static final String PURPLE_BOLD = "\033[1;35m";
 
 	/**
 	 * Cyan bold colored text.
 	 */
-	public static final String CYAN_BOLD = "\033[1;36m";   // CYAN
+	public static final String CYAN_BOLD = "\033[1;36m";
 
 	/**
 	 * White bold colored text.
 	 */
-	public static final String WHITE_BOLD = "\033[1;37m";  // WHITE
+	public static final String WHITE_BOLD = "\033[1;37m";
 
 	/*******************************************************************
 	 * Underline
@@ -131,42 +131,42 @@ public class TermColors {
 	/**
 	 * Black underlined colored text.
 	 */
-	public static final String BLACK_UNDERLINED = "\033[4;30m";  // BLACK
+	public static final String BLACK_UNDERLINED = "\033[4;30m";
 
 	/**
 	 * Red underlined colored text.
 	 */
-	public static final String RED_UNDERLINED = "\033[4;31m";    // RED
+	public static final String RED_UNDERLINED = "\033[4;31m";
 
 	/**
 	 * Green underlined colored text.
 	 */
-	public static final String GREEN_UNDERLINED = "\033[4;32m";  // GREEN
+	public static final String GREEN_UNDERLINED = "\033[4;32m";
 
 	/**
 	 * Yellow underlined colored text.
 	 */
-	public static final String YELLOW_UNDERLINED = "\033[4;33m"; // YELLOW
+	public static final String YELLOW_UNDERLINED = "\033[4;33m";
 
 	/**
 	 * Blue underlined colored text.
 	 */
-	public static final String BLUE_UNDERLINED = "\033[4;34m";   // BLUE
+	public static final String BLUE_UNDERLINED = "\033[4;34m";
 
 	/**
 	 * White underlined colored text.
 	 */
-	public static final String PURPLE_UNDERLINED = "\033[4;35m"; // PURPLE
+	public static final String PURPLE_UNDERLINED = "\033[4;35m";
 
 	/**
 	 * White underlined colored text.
 	 */
-	public static final String CYAN_UNDERLINED = "\033[4;36m";   // CYAN
+	public static final String CYAN_UNDERLINED = "\033[4;36m";
 
 	/**
 	 * White underlined colored text.
 	 */
-	public static final String WHITE_UNDERLINED = "\033[4;37m";  // WHITE
+	public static final String WHITE_UNDERLINED = "\033[4;37m";
 
 	/*******************************************************************
 	 * Background
@@ -219,68 +219,130 @@ public class TermColors {
 	/**
 	 * Black brightly colored text.
 	 */
-	public static final String BLACK_BRIGHT = "\033[0;90m";  // BLACK
+	public static final String BLACK_BRIGHT = "\033[0;90m";
 
 	/**
 	 * Red brightly colored text.
 	 */
-	public static final String RED_BRIGHT = "\033[0;91m";    // RED
+	public static final String RED_BRIGHT = "\033[0;91m";
 
 	/**
 	 * Green brightly colored text.
 	 */
-	public static final String GREEN_BRIGHT = "\033[0;92m";  // GREEN
+	public static final String GREEN_BRIGHT = "\033[0;92m";
 
 	/**
 	 * Yellow brightly colored text.
 	 */
-	public static final String YELLOW_BRIGHT = "\033[0;93m"; // YELLOW
+	public static final String YELLOW_BRIGHT = "\033[0;93m";
 
 	/**
 	 * Blue brightly colored text.
 	 */
-	public static final String BLUE_BRIGHT = "\033[0;94m";   // BLUE
+	public static final String BLUE_BRIGHT = "\033[0;94m";
 
 	/**
 	 * Purple brightly colored text.
 	 */
-	public static final String PURPLE_BRIGHT = "\033[0;95m"; // PURPLE
+	public static final String PURPLE_BRIGHT = "\033[0;95m";
 
 	/**
 	 * Cyan brightly colored text.
 	 */
-	public static final String CYAN_BRIGHT = "\033[0;96m";   // CYAN
+	public static final String CYAN_BRIGHT = "\033[0;96m";
 
 	/**
 	 * White brightly colored text.
 	 */
-	public static final String WHITE_BRIGHT = "\033[0;97m";  // WHITE
+	public static final String WHITE_BRIGHT = "\033[0;97m";
 
 	/*******************************************************************
 	 * Bold High Intensity
 	 *******************************************************************/
 
-	public static final String BLACK_BOLD_BRIGHT = "\033[1;90m"; // BLACK
-	public static final String RED_BOLD_BRIGHT = "\033[1;91m";   // RED
-	public static final String GREEN_BOLD_BRIGHT = "\033[1;92m"; // GREEN
-	public static final String YELLOW_BOLD_BRIGHT = "\033[1;93m";// YELLOW
-	public static final String BLUE_BOLD_BRIGHT = "\033[1;94m";  // BLUE
-	public static final String PURPLE_BOLD_BRIGHT = "\033[1;95m";// PURPLE
-	public static final String CYAN_BOLD_BRIGHT = "\033[1;96m";  // CYAN
-	public static final String WHITE_BOLD_BRIGHT = "\033[1;97m"; // WHITE
+	/**
+	 * Black brightly colored bold text.
+	 */
+	public static final String BLACK_BOLD_BRIGHT = "\033[1;90m";
+
+	/**
+	 * Red brightly colored bold text.
+	 */
+	public static final String RED_BOLD_BRIGHT = "\033[1;91m";
+
+	/**
+	 * Green brightly colored bold text.
+	 */
+	public static final String GREEN_BOLD_BRIGHT = "\033[1;92m";
+
+	/**
+	 * Yellow brightly colored bold text.
+	 */
+	public static final String YELLOW_BOLD_BRIGHT = "\033[1;93m";
+
+	/**
+	 * Blue brightly colored bold text.
+	 */
+	public static final String BLUE_BOLD_BRIGHT = "\033[1;94m";
+
+	/**
+	 * Purple brightly colored bold text.
+	 */
+	public static final String PURPLE_BOLD_BRIGHT = "\033[1;95m";
+
+	/**
+	 * Cyan brightly colored bold text.
+	 */
+	public static final String CYAN_BOLD_BRIGHT = "\033[1;96m";
+
+	/**
+	 * White brightly colored bold text.
+	 */
+	public static final String WHITE_BOLD_BRIGHT = "\033[1;97m";
 
 	/*******************************************************************
 	 * High Intensity backgrounds
 	 *******************************************************************/
 
-	public static final String BLACK_BACKGROUND_BRIGHT = "\033[0;100m";// BLACK
-	public static final String RED_BACKGROUND_BRIGHT = "\033[0;101m";// RED
-	public static final String GREEN_BACKGROUND_BRIGHT = "\033[0;102m";// GREEN
-	public static final String YELLOW_BACKGROUND_BRIGHT = "\033[0;103m";// YELLOW
-	public static final String BLUE_BACKGROUND_BRIGHT = "\033[0;104m";// BLUE
-	public static final String PURPLE_BACKGROUND_BRIGHT = "\033[0;105m"; // PURPLE
-	public static final String CYAN_BACKGROUND_BRIGHT = "\033[0;106m";  // CYAN
-	public static final String WHITE_BACKGROUND_BRIGHT = "\033[0;107m";   // WHITE
+	/**
+	 * Black brightly colored background.
+	 */
+	public static final String BLACK_BACKGROUND_BRIGHT = "\033[0;100m";
+
+	/**
+	 * Red brightly colored background.
+	 */
+	public static final String RED_BACKGROUND_BRIGHT = "\033[0;101m";
+
+	/**
+	 * Green brightly colored background.
+	 */
+	public static final String GREEN_BACKGROUND_BRIGHT = "\033[0;102m";
+
+	/**
+	 * Yellow brightly colored background.
+	 */
+	public static final String YELLOW_BACKGROUND_BRIGHT = "\033[0;103m";
+
+	/**
+	 * Blue brightly colored background.
+	 */
+	public static final String BLUE_BACKGROUND_BRIGHT = "\033[0;104m";
+
+	/**
+	 * Purple brightly colored background.
+	 */
+	public static final String PURPLE_BACKGROUND_BRIGHT = "\033[0;105m";
+
+	/**
+	 * Cyan brightly colored background.
+	 */
+	public static final String CYAN_BACKGROUND_BRIGHT = "\033[0;106m";
+
+	/**
+	 * White brightly colored background.
+	 */
+	public static final String WHITE_BACKGROUND_BRIGHT = "\033[0;107m";
 
 	/* TODO Use me, 3 methods below */
 
