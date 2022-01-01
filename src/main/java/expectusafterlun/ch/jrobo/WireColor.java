206b64abecf62990c4ef8d467ce8ea53a6dbe04e
Christopher Lemire <goodbye300@aim.com>
2022-01-01 06:05:28 -0600

    Updated class-path

---
+++ src/main/java/expectusafterlun/ch/jrobo/WireColor.java
@@ -0,0 +1,60 @@
+/*
+ * JRobo - An Advanced IRC Bot written in Java
+ *
+ * Copyright (C) <2013> <Christopher Lemire>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package expectusafterlun.ch.jrobo;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Purpose: Move all Bomb/Wire code out of BotCommand to here
+ * 
+ * @author Chris Lemire {@literal <goodbye300@aim.com>}
+ */
+public enum WireColor {
+
+  /**
+   * Red colored wire.
+   */
+  RED, 
+
+  /**
+   * Green colored wire.
+   */
+  GREEN, 
+
+  /**
+   * Blue colored wire.
+   */
+  BLUE;
+
+  private static final List<WireColor> VALUES =
+          Collections.unmodifiableList(Arrays.asList(values()));
+  private static final int SIZE = VALUES.size();
+  private static final Random RANDOM = new Random();
+
+  /**
+   * @return A random wire color
+   */
+  public static WireColor randomColor() {
+    return VALUES.get(RANDOM.nextInt(SIZE));
+  }
+}
