f41cb20d62cb0c41b1f1e38f349c2fdc179a4006
Christopher Lemire <goodbye300@aim.com>
2022-01-01 06:39:12 -0600

    Great success!!!

--- src/main/java/expectusafterlun/ch/jrobo/FileReader.java
+++ src/main/java/expectusafterlun/ch/jrobo/FileReader.java
@@ -16,14 +16,16 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  */
-
 package expectusafterlun.ch.jrobo;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import static java.lang.System.err;
 import static java.lang.System.out;
 import java.util.ArrayList;
@@ -33,157 +35,165 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * FileReader is used to supply the Config class with configuration read in from Config.json. It also reads in Jokes JRobo uses and shuffles them in random order.
- * 
+ * FileReader is used to supply the Config class with configuration read in from
+ * Config.json. It also reads in Jokes JRobo uses and shuffles them in random
+ * order.
+ *
  * @author Christopher Lemire {@literal <goodbye300@aim.com>}
  */
 public class FileReader {
 
-    /* Standard Java API Classes */
-    private static String CONFIGFILE;
+	/* Standard Java API Classes */
+	private final static String CONFIGFILE = "Config.json";
 
-    /* Path to resources */
-    private static final String PATH = "src/main/resources/jrobo/expectusafterlun/ch/";
+	/* Path to resources */
+	private final static String PATH = "src/main/resources/jrobo/expectusafterlun/ch/";
 
-    /* User-defined Classes */
-    private static Config config;
+	/* User-defined Classes */
+	private static Config config;
 
-    /* Miscellaneous */
-    private static boolean ranOnce = false;
+	/* Miscellaneous */
+	private static boolean ranOnce = false;
 
-    /**
-     * Set the filename for CONFIGFILE and initialize config to null
-     * 
-     * @author Chris Lemire {@literal <goodbye300@aim.com>}
-     */
-    public FileReader() {
-        CONFIGFILE = "Config.json";
-        config = null;
-    }
+	/**
+	 * Set the filename for CONFIGFILE and initialize config to null
+	 *
+	 * @author Chris Lemire {@literal <goodbye300@aim.com>}
+	 */
+	public FileReader() {
+		config = null;
+	}
 
-    /**
-     * Opens a resource file inside the package and fills the passed ArrayList
-     *
-     * @author Chris Lemire {@literal <goodbye300@aim.com>}
-     * @param FILENAME The file name inside the package to be opened
-     * @param LISTARR The array list to store the file lines to
-     * @return A true on success and false on failure
-     */
-    protected boolean fileToArrayList(final String FILENAME, final ArrayList<String> LISTARR) {
-        out.println( TermColors.colorInfo("Reading File (" + PATH + FILENAME + ")") );
-        out.println( TermColors.colorInfo("Absolute Path: " + new File(PATH + FILENAME).getAbsolutePath()) );
-        out.println( TermColors.colorInfo("System User Directory: " + PATH + System.getProperty("user.dir")) );
-        out.println( TermColors.colorInfo("Reading File (" + PATH + FILENAME + ")") );
+	/**
+	 * Opens a resource file inside the package and fills the passed
+	 * ArrayList
+	 *
+	 * @author Chris Lemire {@literal <goodbye300@aim.com>}
+	 * @param FILENAME The file name inside the package to be opened
+	 * @param LISTARR The array list to store the file lines to
+	 * @return A true on success and false on failure
+	 */
+	protected boolean fileToArrayList(final String FILENAME, final ArrayList<String> LISTARR) {
+		out.println(TermColors.colorInfo("Reading File (" + PATH + FILENAME + ")"));
+		out.println(TermColors.colorInfo("Absolute Path: " + new File(PATH + FILENAME).getAbsolutePath()));
+		out.println(TermColors.colorInfo("System User Directory: " + PATH + System.getProperty("user.dir")));
+		out.println(TermColors.colorInfo("Reading File (" + PATH + FILENAME + ")"));
 
-        try {
-            File file = new File(PATH + FILENAME);
-                try (Scanner myReader = new Scanner(file)) {
-                    if (FILENAME.equals(PATH + CONFIGFILE)) {
-                        ranOnce = true;
-                }
-			
-                while ( myReader.hasNextLine() ) {
-                    LISTARR.add(myReader.nextLine());
-                }
-            }
+		try {
+			File file = new File(PATH + FILENAME);
+			try (Scanner myReader = new Scanner(file)) {
+				if (FILENAME.equals(PATH + CONFIGFILE)) {
+					ranOnce = true;
+				}
 
-        } catch (IOException ex) {
-            Logger.getLogger(FileReader.class.getName()).log(Level.SEVERE, null, ex);
+				while (myReader.hasNextLine()) {
+					LISTARR.add(myReader.nextLine());
+				}
+			}
 
-        }
+		} catch (IOException ex) {
+			Logger.getLogger(FileReader.class.getName()).log(Level.SEVERE, null, ex);
 
-        Collections.shuffle(LISTARR);
+		}
 
-        return true;
-    }
+		Collections.shuffle(LISTARR);
 
-    /**
-     * Gets the data from the configuration file
-     *
-     * @return Returns a Config object, with the settings from Config.json
-     * @since 2013-02-18
-     */
-    protected static Config getConfig() {
+		return true;
+	}
 
-        /* 
-         * Should only be null if this method has not already been ran once 
-         * Or the constructor has never been called
-         */
-        if (config != null || ranOnce) {
-            //Thread.dumpStack();
-            out.println(TermColors.colorInfo("Reusing Config because it's != null or " + PATH + CONFIGFILE + " has already been read once"));
-            return config;
-        }
+	/**
+	 * Gets the data from the configuration file
+	 *
+	 * @return Returns a Config object, with the settings from Config.json
+	 * @since 2013-02-18
+	 */
+	protected Config getConfig() {
 
-        out.println(TermColors.colorInfo("Reading Configuration File (" + PATH.concat(CONFIGFILE) + ")"));
-        out.println(TermColors.colorInfo("Absolute path: " + new File( (PATH.concat(CONFIGFILE)) ).getAbsolutePath()));
-        out.println(TermColors.colorInfo("System user directory: " + System.getProperty("user.dir").concat(PATH)));
-        
-        try {
-            File file = new File(PATH.concat(CONFIGFILE));
-            String json;
-//            Thread.dumpStack();
-            try (Scanner myReader = new Scanner(file)) {
-                json = "";
-			
-                while ( myReader.hasNextLine() ) {
-                    json += myReader.nextLine();
-                }
-        }
-            
-            Gson gson = new Gson();
-            config = gson.fromJson(json, Config.class);
+		/* 
+         		 * Should only be null if this method has not already been ran once 
+		 * Or the constructor has never been called
+		 */
+		if (config != null || ranOnce) {
+			//Thread.dumpStack();
+			out.println(TermColors.colorInfo("Reusing Config because it's != null or " + PATH + CONFIGFILE + " has already been read once"));
+			return config;
+		}
 
-            ranOnce = true;
+		out.println(TermColors.colorInfo("Reading Configuration File (" + PATH.concat(CONFIGFILE) + ")"));
+		out.println(TermColors.colorInfo("Absolute path: " + new File((PATH.concat(CONFIGFILE))).getAbsolutePath()));
+		out.println(TermColors.colorInfo("System user directory: " + System.getProperty("user.dir").concat(PATH)));
 
-        } catch (JsonSyntaxException | NullPointerException | FileNotFoundException ex) {
-            Logger.getLogger(FileReader.class.getName()).log(Level.SEVERE, null, ex);
-            System.exit(1);
-        }
+		try {
+			//InputStream in = getClass().getResourceAsStream("/" + CONFIGFILE);
+			//BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 
-        // Verifiying important settings for connection
-        if (config.getName()
-                == null) {
-            err.println(TermColors.colorInfo("Error: Unable to find bot's Nickname"));
-            System.exit(1);
-        }
+			//Thread.dumpStack();
 
-        if (config.getPass()
-                == null) {
-            err.println(TermColors.colorInfo("Error: Unable to find bot's Password"));
-            System.exit(1);
-        }
+			String json = null;
 
-        if (config.getMasters()
-                == null) {
-            err.println(TermColors.colorInfo("Error: Unable to find bot's Masters"));
-            System.exit(1);
-        }
+			try (BufferedReader reader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/" + CONFIGFILE)))) {
+				json = "";
 
-        if (config.getCmdSymb()
-                == '\u0000') {
-            err.println(TermColors.colorInfo("Error: Unable to find bot's Command Symbol"));
-            System.exit(1);
-        }
+				while (reader.ready()) {
+					json += reader.readLine();
+				}
+			} catch (IOException ex) {
+				Logger.getLogger(FileReader.class.getName()).log(Level.SEVERE, null, ex);
+			}
 
-        if (config.getNetwork()
-                == null) {
-            err.println(TermColors.colorInfo("Error: Unable to find bot's Network"));
-            System.exit(1);
-        }
+			Gson gson = new Gson();
+			config = gson.fromJson(json, Config.class);
 
-        if (config.getChannel()
-                == null) {
-            err.println(TermColors.colorInfo("Error: Unable to find bot's Channel"));
-            System.exit(1);
-        }
+			ranOnce = true;
 
-        if (config.getOpenWeatherMapKey()
-                == null) {
-            err.println(TermColors.colorInfo("Error: Unable to find bot's OpenWeatherMap API Key"));
-            //System.exit(1);
-        }
+		} catch (JsonSyntaxException | NullPointerException  ex) {
+			Logger.getLogger(FileReader.class.getName()).log(Level.SEVERE, null, ex);
+			System.exit(1);
+		}
 
-        return config;
-    } // EOF getConfig()
+		// Verifiying important settings for connection
+		if (config.getName()
+			== null) {
+			err.println(TermColors.colorInfo("Error: Unable to find bot's Nickname"));
+			System.exit(1);
+		}
+
+		if (config.getPass()
+			== null) {
+			err.println(TermColors.colorInfo("Error: Unable to find bot's Password"));
+			System.exit(1);
+		}
+
+		if (config.getMasters()
+			== null) {
+			err.println(TermColors.colorInfo("Error: Unable to find bot's Masters"));
+			System.exit(1);
+		}
+
+		if (config.getCmdSymb()
+			== '\u0000') {
+			err.println(TermColors.colorInfo("Error: Unable to find bot's Command Symbol"));
+			System.exit(1);
+		}
+
+		if (config.getNetwork()
+			== null) {
+			err.println(TermColors.colorInfo("Error: Unable to find bot's Network"));
+			System.exit(1);
+		}
+
+		if (config.getChannel()
+			== null) {
+			err.println(TermColors.colorInfo("Error: Unable to find bot's Channel"));
+			System.exit(1);
+		}
+
+		if (config.getOpenWeatherMapKey()
+			== null) {
+			err.println(TermColors.colorInfo("Error: Unable to find bot's OpenWeatherMap API Key"));
+			//System.exit(1);
+		}
+
+		return config;
+	} // EOF getConfig()
 } // EOF class
